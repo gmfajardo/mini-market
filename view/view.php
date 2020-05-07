f62e85a7177a909a9f9a70c75d90ed9399f6082f
fazt <A19A88603@educacion.edu.do>
2020-05-07 16:03:52 -0400

    nueva actualizacion

---
+++ view/view.php
@@ -0,0 +1,42 @@
+foreach ($usuarios as $user){
+                      <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+        <table  >
+        row
+            <thead>
+                <tr>
+                    <td>Usuario</td>
+                    <td>Contraseña</td>
+                    <td>Nombre</td>
+                </tr>
+            </thead>
+
+            <tbody>
+            <tr>
+                    <td>bria</td>
+                    <td>tumadrina</td>
+                    <td>qloq</td>
+                </tr>
+
+                <?php  echo "<tr>";
+                        echo "<td>". $user['username']."</td>";
+                        echo "<td>". $user['password']."</td>";
+                        echo "<td>". $user['nombre']."</td>";
+                        echo "</tr>";
+                    }
+                
+                ?>
+            </tbody>
+
+        </table>
+
+
+</body>
+</html>
